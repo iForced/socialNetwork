a66f1da9d4e4d4a9ee116c65aa64327ff7f0df05
iForced <iforce@2ch.hk>
2021-09-01 00:13:36 +0300

    request and render photos from jsonplaceholder (many errors)

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -7,42 +7,82 @@ type TodoType = {
     title: string
     completed: boolean
 }
-type TodosList = Array<TodoType>
+type TodosListType = Array<TodoType>
+// {albumId: 1, id: 1, title: "accusamus beatae ad facilis cum similique qui sunt",
+// url: "https://via.placeholder.com/600/92c952", thumbnailUrl: "https://via.placeholder.com/150/92c952"}
+
+type PhotoType = {
+    albumId: number
+    id: number
+    title: string
+    url: string
+    thumbnailUrl: string
+}
+type PhotosListType = Array<PhotoType>
 
 function News() {
-    // const getTodos = async () => {
-    //     const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-    //     const todos: TodosList = await response.json()
-    //     return todos.splice(0, 10).map(td => td.title)
-    //     // console.log(todos)
-    // }
-    // getTodos()
 
-    const [state, setState] = useState<TodosList>([])
+    const [todos, setTodos] = useState<TodosListType>([])
+    const [photos, setPhotos] = useState<PhotosListType>([])
+
+    //TODO разобраться с ошибками после выполнения fetch
 
     fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-        })
         .then(
-            (data) => {
-                setState([...data.splice(0, 10)])
+            (response) => {
+                if (response.ok) {
+                    return response.json()
+                }
             }
         )
-        .catch(err => console.log(err))
+        .then(
+            (data) => {
+                setTodos([...data.splice(0, 10)])
+            }
+        )
+        .catch(err => console.log(err + ' in todos request'))
 
-    const todosElement = state.map(td => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(
+            (response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+            }
+        )
+        .then(
+            (data) => {
+                setPhotos([...data.splice(0, 10)])
+                // console.log(data[0])
+            })
+        .catch(
+            (err) => {
+                console.log(err + ' in photos request')
+            }
+        )
+
+
+    const todosElement = todos.map(td => {
         return <li key={td.id} className={s.list_item + ' ' + (!td.completed && s.uncompleted)}>
             <div>{td.title}</div>
         </li>
     })
+    const photosElement = photos.map(ph => {
+        return <li key={ph.id}>
+            <p>{ph.title}</p>
+            <img src={ph.url} alt=""/>
+        </li>
+    })
 
     return (
-        <ul className={s.list}>
-            {todosElement}
-        </ul>
+        <>
+            <ul className={s.list}>
+                {todosElement}
+            </ul>
+            <ul>
+                {photosElement}
+            </ul>
+        </>
     )
 }
 
