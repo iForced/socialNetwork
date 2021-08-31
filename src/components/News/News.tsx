7c4079858e895655ff0bba7d6104f6f0719e9f30
iForced <iforce@2ch.hk>
2021-08-31 22:19:52 +0300

    practice async request to jsonplaceholder in News component and render data on screen

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import s from './News.module.css'
 
+type TodoType = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
+type TodosList = Array<TodoType>
+
 function News() {
+    // const getTodos = async () => {
+    //     const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    //     const todos: TodosList = await response.json()
+    //     return todos.splice(0, 10).map(td => td.title)
+    //     // console.log(todos)
+    // }
+    // getTodos()
+
+    const [state, setState] = useState<TodosList>([])
+
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+        })
+        .then(
+            (data) => {
+                setState([...data.splice(0, 10)])
+            }
+        )
+        .catch(err => console.log(err))
+
+    const todosElement = state.map(td => {
+        return <li key={td.id} className={s.list_item + ' ' + (!td.completed && s.uncompleted)}>
+            <div>{td.title}</div>
+        </li>
+    })
+
     return (
-        <div>
-            News
-        </div>
+        <ul className={s.list}>
+            {todosElement}
+        </ul>
     )
 }
 
