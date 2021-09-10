c6436c70baccf5529b8b32a2fa68b027cb760406
iForced <iforce@2ch.hk>
2021-09-10 14:46:47 +0300

    rewrite users functional component to class

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,38 +4,43 @@ import {UsersPropsType} from "./UsersContainer";
 import axios from "axios";
 import avatar from '../../assets/avatar.png'
 
-function Users(props: UsersPropsType) {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+class Users extends React.Component<UsersPropsType> {
+    constructor(props: UsersPropsType) {
+        super(props);
+
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => props.setUsers(response.data.items))
     }
 
-    const usersElement = props.users.map(u =>
-        <div key={u.id} className={s.user}>
-            <div className={s.user_avatar}>
-                <div className={s.avatar_image}>
-                    <img src={u.photos.small || avatar} alt="avatar"/>
+    render() {
+        const usersElement = this.props.users.map(u =>
+            <div key={u.id} className={s.user}>
+                <div className={s.user_avatar}>
+                    <div className={s.avatar_image}>
+                        <img src={u.photos.small || avatar} alt="avatar"/>
+                    </div>
+                    <button
+                        className={s.follow_button}
+                        onClick={() => u.followed ? this.props.unfollow : this.props.follow}
+                    >{u.followed ? 'Unfollow' : 'Follow'}</button>
                 </div>
-                <button
-                    className={s.follow_button}
-                    onClick={() => u.followed ? props.unfollow : props.follow}
-                >{u.followed ? 'Unfollow' : 'Follow'}</button>
+                <div className={s.user_info}>
+                    <div className={s.user_name}>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </div>
+                    <div className={s.user_location}>
+                        {'location'}
+                    </div>
+                </div>
+            </div>)
+        return (
+            <div className={s.users_container}>
+                {usersElement}
             </div>
-            <div className={s.user_info}>
-                <div className={s.user_name}>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </div>
-                <div className={s.user_location}>
-                    {'location'}
-                </div>
-            </div>
-        </div>)
-    return (
-        <div className={s.users_container}>
-            {usersElement}
-        </div>
-    )
+        )
+    }
 }
 
 export default Users
